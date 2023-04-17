daf372d78b34051f011173005abd1851d3a19612
Mickael R <94828882+Micka0675@users.noreply.github.com>
2023-04-17 11:34:18 +0200

    Add workin'class 'MJoueur'

---
+++ MJoueur.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morpion
+{
+    internal class MJoueur
+    {
+        private string nomJoueur;
+
+        public string NomJoueur
+        {
+            get { return nomJoueur; }
+            set { nomJoueur = value; }
+        }
+
+        private string prenomJoueur;
+
+        public string PrenomJoueur 
+        {
+            get { return prenomJoueur; }
+            set { nomJoueur= value; }
+        }
+
+        private string pseudoJoueur;
+
+        public string PseudoJoueur
+        { 
+            get { return pseudoJoueur; }
+            set { pseudoJoueur= value; }
+        }
+
+        internal MJoueur(string consNomJoueur,string consPrenomJoueur,string consPseudoJoueur) 
+        { 
+            NomJoueur = consNomJoueur;
+            PrenomJoueur = consPrenomJoueur;
+            PseudoJoueur = consPseudoJoueur;
+        }
+    }
+}
